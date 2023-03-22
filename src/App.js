eda7b9408ee380df8e14caba5344bf9182f7834f
Imranhossain7 <imran.ice.nstu12@gmail.com>
2023-03-23 00:01:30 +0600

    Initial setup & API called successfully

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const handleAPI = () => {
+    fetch("https://randomuser.me/api/?results=5000").then((res) =>
+      res.json().then((data) => {
+        console.log(data.results);
+      })
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3> Random User generator</h3>
+      <button onClick={handleAPI}>Call the API</button>
     </div>
   );
 }
